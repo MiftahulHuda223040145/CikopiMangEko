8194a9481a78440ebd20eae9d623fde92a4cc59c
MiftahulHuda223040145 <heromiftah2004@gmail.com>
2025-07-19 18:49:51 +0700

    Update Hover

--- resources/views/about-us/about-us.blade.php
+++ resources/views/about-us/about-us.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
 @section('title', 'About Us')
 {{-- Hero --}}
-<section class="bg-center bg-no-repeat pb-10" style="background-image: url('{{ asset('storage/image/hero-image-aboutus.png') }}')">
-    <div class="px-4 max-w-screen-xl place-items-start text-left pt-56 fade-hero">
+<section class="bg-center bg-no-repeat" style="background-image: url('{{ asset('storage/image/hero-image-aboutus.png') }}')">
+    <div class="px-4 place-items-start text-left pt-56 pb-10 fade-hero lg:backdrop-brightness-100 md:backdrop-brightness-50 sm:backdrop-brightness-50 max-[640px]:backdrop-brightness-50 max-w-screen">
         <div class="pt-60 lg:pl-15 md:pl-15 sm:px-10 max-[640px]:px-10">
             <h1 class="mb-4 text-4xl font-ppbold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Cikopi</h1>
             <h1 class="mb-4 text-4xl font-ppbold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Mang Eko</h1>
@@ -14,33 +14,33 @@
 {{-- About Our Team --}}
 <div class="container lg:px-25 md:px-10 sm:px-10 max-[640px]:px-10 max-w-screen font-lato fade-in-up">
     <div class="font-lato max-w-screen-xl place-items-start text-left mt-15 mb-3">
-        <h1 class="lg:text-3xl md:text-3xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">About Our Team</h1>
+        <h1 class="lg:text-4xl md:text-4xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">About Our Team</h1>
     </div>
     <div class="py-3 justify-center max-w-screen gap-8 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 max-[640px]:grid-cols-2 mb-25">
         <div class="">
             <div class="place-items-center">
-                <img class="rounded-full w-2xs" src="{{ asset('storage/image/muchtar-koswara.png') }}" alt="">
+                <img class="rounded-full w-2xs transform transition duration-500 ease-out hover:scale-105" src="{{ asset('storage/image/muchtar-koswara.png') }}" alt="">
             </div>
             <h2 class="font-bold lg:text-xl md-text-xl sm:text-base pt-10">Muchtar Koswara</h2>
             <p class="text-left lg:text-base md:text-sm sm:text-sm max-[640px]:text-xs">Owner Cikopi Mang Eko</p>       
         </div>
         <div class="">
             <div class="place-items-center">
-                <img class="rounded-full w-2xs" src="{{ asset('storage/image/dodi-setiadi.png') }}" alt="">
+                <img class="rounded-full w-2xs transform transition duration-500 ease-out hover:scale-105" src="{{ asset('storage/image/dodi-setiadi.png') }}" alt="">
             </div>
             <h2 class="font-bold lg:text-xl md-text-xl sm:text-base pt-10">Dodi Setiadi</h2>
             <p class="text-left lg:text-base md:text-sm sm:text-sm max-[640px]:text-xs">Store Manager</p>       
         </div>
         <div class="">
             <div class="place-items-center">
-                <img class="rounded-full w-2xs" src="{{ asset('storage/image/novi-indriawan.png') }}" alt="">
+                <img class="rounded-full w-2xs transform transition duration-500 ease-out hover:scale-105" src="{{ asset('storage/image/novi-indriawan.png') }}" alt="">
             </div>
             <h2 class="font-bold lg:text-xl md-text-xl sm:text-base pt-10">Novi Indriawan</h2>
             <p class="text-left lg:text-base md:text-sm sm:text-sm max-[640px]:text-xs">Production Manager</p>       
         </div>
         <div class="">
             <div class="place-items-center">
-                <img class="rounded-full w-2xs" src="{{ asset('storage/image/bunga-utami.png') }}" alt="">
+                <img class="rounded-full w-2xs transform transition duration-500 ease-out hover:scale-105" src="{{ asset('storage/image/bunga-utami.png') }}" alt="">
             </div>
             <h2 class="font-bold lg:text-xl md-text-xl sm:text-base pt-10">Bunga Utami</h2>
             <p class="text-left lg:text-base md:text-sm sm:text-sm max-[640px]:text-xs">Sales and Marketing</p>       
@@ -48,15 +48,22 @@
     </div>
 </div>
 
-<div class="container max-w-screen px-25 font-lato fade-in-up">
-    <div class="font-lato max-w-screen-xl place-items-start text-left mt-15 mb-3">
-        <h1 class="text-3xl font-bold text-[#39312F]">Legalities & Certificate</h1>
-    </div>
-    <div class="py-3 justify-center max-w-screen gap-4 grid grid-cols-4 mb-25">
-        <img class="rounded-4xl w-2xs" src="{{ asset('storage/image/foto-acehgayo.png') }}" alt="">
-        <img class="rounded-4xl w-2xs" src="{{ asset('storage/image/foto-acehgayo.png') }}" alt="">
-        <img class="rounded-4xl w-2xs" src="{{ asset('storage/image/foto-acehgayo.png') }}" alt="">
-        <img class="rounded-4xl w-2xs" src="{{ asset('storage/image/foto-acehgayo.png') }}" alt="">
+<div class="container max-w-screen lg:px-25 md:px-10 sm:px-10 max-[640px]:px-10 my-40 font-lato fade-in-up">
+    <h1 class="lg:text-4xl md:text-4xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F] mb-10">Visi & Misi</h1>
+    <div class="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-20">
+        <div class="">
+            <h1 class="lg:text-4xl md:text-4xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Visi</h1>
+            <p class="my-5 lg:text-base md:text-sm sm:text-sm max-[640px]:text-xs">Menjadi penyedia biji kopi premium dari seluruh kepulauan di Indonesia yang mengedepankan kualitas, keberlanjutan, dan kesejahteraan petani lokal, serta memberikan pengalaman kopi terbaik pada komsumen global.</p>
+        </div>
+        <div class="">
+            <h1 class="lg:text-4xl md:text-4xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Misi</h1>
+            <ol class="list-decimal list-inside my-5 lg:text-base md:text-sm sm:text-sm max-[640px]:text-xs">
+                <li class="pb-4">Memberikan edukasi kepada para penggiat kopi tentang proses pengolahan kopi yang baik dan benar.</li>
+                <li class="pb-4">Mempromosikan komoditas kopi yang berasal dari tiap daerah di Indonesia, khusunya di kota Bandung.</li>
+                <li class="pb-4">Menjamin penggunaan bahan baku (green bean) yang berkualitas.</li>
+                <li class="pb-4">Meningkatkan budaya ngopi sehat di Masyarakat.</li>
+            </ol>
+        </div>
     </div>
 </div>
 </x-layout>
--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -15,14 +15,14 @@
 <div class="container max-w-screen lg:px-25 md:px-10 sm:px-10 max-[640px]:px-10 my-15 font-lato fade-in-up">
     <div class="font-lato lg:gap-40 md:gap-10 sm:gap-10 max-[640px]:gap-10 justify-center flex lg:flex-row md:flex-row sm:flex-col max-[640px]:flex-col place-items-start mx-auto max-w-screen-xl text-left py-3">
         <div class="">
-            <img class="w-90 rounded-3xl" src="{{ asset('storage/image/green-bean.png') }}" alt="">
+            <img class="w-90 rounded-3xl transform transition duration-500 ease-out hover:scale-105" src="{{ asset('storage/image/green-bean.png') }}" alt="">
             <div class="py-3">
                 <h2 class="font-bold lg:text-xl md-text-xl sm:text-base">Grean Bean</h2>
                 <p class="lg:w-45 md:w-45 sm:w-auto lg:text-base md:text-sm sm:text-sm max-[640px]:text-xs">Rp. 50.000 - Rp. 90.000</p>  
             </div>
         </div>
         <div class="">
-            <h1 class="lg:text-3xl md:text-3xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Green Bean</h1>
+            <h1 class="lg:text-4xl md:text-4xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Green Bean</h1>
             <p class="lg:w-170 md:w-auto my-5 text-justify lg:text-xl md-text-xl sm:text-base">Green Bean merupakan biji kopi mentah yang belum disangrai. Green Bean merupakah bahan utama dalam pembuatan kopi, dan kualitasnya menentukan cita rasa akhir kopi yang dihasilkan. Cikopi mang eko menyediakan green bean dari berbagai daerah atau gunung yang ada di Indonesia.</p>
         </div>
     </div>
@@ -30,11 +30,11 @@
 <div class="container max-w-screen lg:px-25 md:px-10 sm:px-10 max-[640px]:px-10 my-15 fade-in-up">
     <div class="font-lato lg:gap-40 md:gap-10 sm:gap-10 max-[640px]:gap-10 justify-center flex lg:flex-row md:flex-row sm:flex-col-reverse max-[640px]:flex-col-reverse place-items-start mx-auto max-w-screen-xl text-left py-3">
         <div class="">
-            <h1 class="lg:text-3xl md:text-3xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Roasted Bean</h1>
+            <h1 class="lg:text-4xl md:text-4xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Roasted Bean</h1>
             <p class="lg:w-150 md:w-auto my-5 text-justify lg:text-xl md-text-xl sm:text-base">Green Bean merupakan biji kopi yang di panggang atau sangrai, proses ini bertujuan untuk mengeluarkan aroma, dan roasted bean siap untuk digiling dan diseduh. Cikopi mang eko menyediakan roasted bean dari aceh hingga papua.</p>
         </div>
         <div class="">
-            <img class="w-90 rounded-3xl" src="{{ asset('storage/image/foto-acehgayo.png') }}" alt="">
+            <img class="w-90 rounded-3xl transform transition duration-500 ease-out hover:scale-105" src="{{ asset('storage/image/foto-acehgayo.png') }}" alt="">
             <div class="py-3">
                 <h2 class="font-bold lg:text-xl md-text-xl sm:text-base">Roasted Bean</h2>
                 <p class="lg:w-50 md:w-50 sm:w-auto lg:text-base md:text-sm sm:text-sm max-[640px]:text-xs">Rp. 150.000 - Rp. 190.000</p>       
@@ -45,14 +45,14 @@
 <div class="container max-w-screen lg:px-25 md:px-10 sm:px-10 max-[640px]:px-10 my-15 fade-in-up">
     <div class="font-lato lg:gap-40 md:gap-10 sm:gap-10 max-[640px]:gap-10 justify-center flex lg:flex-row md:flex-row sm:flex-col max-[640px]:flex-col place-items-start mx-auto max-w-screen-xl text-left py-3">
         <div class="">
-            <img class="w-90 rounded-3xl" src="{{ asset('storage/image/roasting-service.png') }}" alt="">
+            <img class="w-90 rounded-3xl transform transition duration-500 ease-out hover:scale-105" src="{{ asset('storage/image/roasting-service.png') }}" alt="">
             <div class="py-3">
                 <h2 class="font-bold lg:text-xl md-text-xl sm:text-base lg:w-45 md:w-45 sm:w-auto">Roasting Service</h2>
                 <p class="lg:text-base md:text-sm sm:text-sm max-[640px]:text-xs">Rp. 25.000/Kg</p>       
             </div>
         </div>
         <div class="">
-            <h1 class="lg:text-3xl md:text-3xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Roasting Service</h1>
+            <h1 class="lg:text-4xl md:text-4xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Roasting Service</h1>
             <p class="lg:w-140 md:w-auto my-5 lg:text-xl md-text-xl sm:text-base text-justify">Roasting Service merupakan jasa yang diberikan oleh cikopi mang eko terhadap konsumen. Adapun service lainnya, seperti jasa giling biji kopi, dan jasa mengajarkan menyeduh kopi secara gratis.</p>
         </div>
     </div>
@@ -60,12 +60,12 @@
 <div class="container max-w-screen lg:px-25 md:px-10 sm:px-10 max-[640px]:px-10 my-15 fade-in-up">
     <div class="font-lato lg:gap-40 md:gap-10 sm:gap-10 max-[640px]:gap-10 justify-center flex lg:flex-row md:flex-row sm:flex-col-reverse max-[640px]:flex-col-reverse place-items-start mx-auto max-w-screen-xl text-left py-3">
         <div class="">
-            <h1 class="lg:text-3xl md:text-3xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Coffee Making Equipment</h1>
+            <h1 class="lg:text-4xl md:text-4xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Coffee Making Equipment</h1>
             <p class="lg:w-150 md:w-auto my-5 text-justify lg:text-xl md-text-xl sm:text-base">Cikopi mang eko menyediakan berbagai alat menyeduh kopi, baik untuk rumahan maupun profesional.</p>
         </div>
         <div class="place-items-end text-left">
             <div class="pr-5">
-                <img class="w-90 rounded-3xl" src="{{ asset('storage/image/coffee-making-equipment.png') }}" alt="">
+                <img class="w-90 rounded-3xl transform transition duration-500 ease-out hover:scale-105" src="{{ asset('storage/image/coffee-making-equipment.png') }}" alt="">
                 <div class="py-3">
                     <h2 class="font-bold lg:text-xl md-text-xl sm:text-base">Coffee Making Equipment</h2>
                     <p class="lg:w-65 md:w-65 sm:w-auto lg:text-base md:text-sm sm:text-sm max-[640px]:text-xs">Rp. 70.000 - Rp. 3.000.000</p>       
@@ -77,14 +77,14 @@
 <div class="container max-w-screen lg:px-25 md:px-10 sm:px-10 max-[640px]:px-10 my-15 fade-in-up">
     <div class="font-lato lg:gap-40 md:gap-10 sm:gap-10 max-[640px]:gap-10 justify-center flex lg:flex-row md:flex-row sm:flex-col max-[640px]:flex-col place-items-start mx-auto max-w-screen-xl text-left py-3">
         <div class="">
-            <img class="w-90 rounded-3xl" src="{{ asset('storage/image/product-hampers.png') }}" alt="">
+            <img class="w-90 rounded-3xl transform transition duration-500 ease-out hover:scale-105" src="{{ asset('storage/image/product-hampers.png') }}" alt="">
             <div class="py-3">
                 <h2 class="font-bold lg:text-xl md-text-xl sm:text-base">Side Product</h2>
                 <p class="lg:w-45 md:w-45 sm:w-auto lg:text-base md:text-sm sm:text-sm max-[640px]:text-xs">Rp. 55.000 - 70.000/L</p>       
             </div>
         </div>
         <div class="">
-            <h1 class="lg:text-3xl md:text-3xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Side Product</h1>
+            <h1 class="lg:text-4xl md:text-4xl sm:text-2xl max-[640px]:text-2xl font-bold text-[#39312F]">Side Product</h1>
             <p class="lg:w-150 md:w-auto my-5 text-justify lg:text-xl md-text-xl sm:text-base">Side product merupakan produk sekunder yang dijual pleh cikopi mang eko, seperti minumana Espresso Concentrate, dan Eskosu (kopi susu).</p>
         </div>
     </div>
